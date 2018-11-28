2dbbd3eb6d524937cd12c0420c8df99fc1567d96
crw-xiix <charles@12strike.com>
2018-11-27 20:41:21 -0800

    Create EmailCreds.h

---
+++ sketch_nov27a/EmailCreds.h
@@ -0,0 +1,10 @@
+#ifndef _EMAIL_CREDS_
+#define _EMAIL_CREDS_
+
+
+#define CREDS_EMAILBASE64_LOGIN "dasdfsdasasdadfasfasdfa"
+#define CREDS_EMAILBASE64_PASSWORD "asqwersdvasdfasdfafdas"
+#define CREDS_FROM "asdfasdfaqwerwadfs@gmail.com"
+
+
+#endif
